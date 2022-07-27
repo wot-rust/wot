4f31da6296b007cba25a0376cac9ea8204e7247c
Luca Barbato <lu_zero@gentoo.org>
2022-07-27 14:22:08 +0200

    doc: Add the wot-rust logo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/sifis-home/wot/master/assets/wot-rust.svg"
+)]
+
 pub use wot_discovery as discovery;
 pub use wot_serve as serve;
 pub use wot_td as td;
